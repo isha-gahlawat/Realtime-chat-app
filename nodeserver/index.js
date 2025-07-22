e0631ee847043deaa990538a574a644c6225f30c
Isha <ishagahlawat@gmail.com>
2025-07-22 15:23:57 +0530

    fixed integration

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,27 +1,40 @@
-//node server handling socket io connection
-const io=require('socket.io')(8000,{
-  cors:{
-    origin:"http://127.0.0.1:5500",
-    methods:["GET","POST"]
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const path = require("path");
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*", // In production, use your actual frontend domain
+    methods: ["GET", "POST"]
   }
 });
 
-const users={};
-//io.on -sare connections ko listen 
-//socket.on ek connection join k bad kya hoga vo
-io.on('connection',socket =>{
-    socket.on('new-user-joined',names=>{
-      console.log("New user",names);
-      users[socket.id]=names;
-      socket.broadcast.emit('user-joined',names) ; 
-    });
-    
-    socket.on('send',message=>{
-     socket.broadcast.emit('receive',{message:message,names:users[socket.id]})   
-    });
+const users = {};
 
-     socket.on('disconnect',message=>{
-     socket.broadcast.emit('left',users[socket.id])
-     delete users[socket.id];
-      })
-    })
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", socket => {
+  socket.on("new-user-joined", name => {
+    console.log("New user", name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  socket.on("send", message => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      names: users[socket.id]
+    });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
